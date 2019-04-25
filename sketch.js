2271859d3e6205f59f49b3e5fe2a6f45b15995ac
danie <danie@DESKTOP-M2H76EP>
2019-04-25 15:35:30 +0200

    now trainable!

--- sketch.js
+++ sketch.js
@@ -3,33 +3,105 @@ ml5 Example
 Webcam Image Classification using MobileNet and p5.js
 This example uses a callback pattern to create the classifier
 === */
-
+let featureExtractor;
 let classifier;
 let video;
-let resultsP;
+let loss;
+let dogImages = 0;
+let catImages = 0;
+
 
 function setup() {
-  noCanvas();
-  // Create a camera input
-  video = createCapture(VIDEO);
-  // Initialize the Image Classifier method with MobileNet and the video as the second argument
-  classifier = ml5.imageClassifier('MobileNet', video, modelReady);
-  resultsP = createP('Loading model and video...');
-}
-
-function modelReady() {
-  console.log('Model Ready');
-  classifyVideo();
-}
-
-// Get a prediction for the current video frame
-function classifyVideo() {
-  classifier.classify(gotResult);
-}
-
-// When we get a result
-function gotResult(err, results) {
-  // The results are in an array ordered by confidence.
-  resultsP.html(results[0].label + ' ' + nf(results[0].confidence, 0, 2));
-  classifyVideo();
-}
+    noCanvas();
+    // Create a video element
+    video = createCapture(VIDEO);
+    // Append it to the videoContainer DOM element
+    video.parent('videoContainer');
+    // Extract the already learned features from MobileNet
+    featureExtractor = ml5.featureExtractor('MobileNet', modelReady);
+    // Create a new classifier using those features and give the video we want to use
+    classifier = featureExtractor.classification(video, videoReady);
+    // Set up the UI buttons
+    setupButtons();
+  }
+  
+  // A function to be called when the model has been loaded
+  function modelReady() {
+    select('#modelStatus').html('Base Model (MobileNet) Loaded!');
+    classifier.load('./model/model.json', function() {
+      select('#modelStatus').html('Custom Model Loaded!');
+    });
+  }
+  
+  // A function to be called when the video has loaded
+  function videoReady () {
+    select('#videoStatus').html('Video ready!');
+  }
+  
+  // Classify the current frame.
+  function classify() {
+    classifier.classify(gotResults);
+  }
+  
+  // A util function to create UI buttons
+  function setupButtons() {
+    // When the Cat button is pressed, add the current frame
+    // from the video with a label of "cat" to the classifier
+    buttonA = select('#catButton');
+    buttonA.mousePressed(function() {
+      classifier.addImage('cat');
+      select('#amountOfCatImages').html(catImages++);
+    });
+  
+    // When the Dog button is pressed, add the current frame
+    // from the video with a label of "dog" to the classifier
+    buttonB = select('#dogButton');
+    buttonB.mousePressed(function() {
+      classifier.addImage('dog');
+      select('#amountOfDogImages').html(dogImages++);
+    });
+  
+    // Train Button
+    train = select('#train');
+    train.mousePressed(function() {
+      classifier.train(function(lossValue) {
+        if (lossValue) {
+          loss = lossValue;
+          select('#loss').html('Loss: ' + loss);
+        } else {
+          select('#loss').html('Done Training! Final Loss: ' + loss);
+        }
+      });
+    });
+  
+    // Predict Button
+    buttonPredict = select('#buttonPredict');
+    buttonPredict.mousePressed(classify);
+  
+    // Save model
+    saveBtn = select('#save');
+    saveBtn.mousePressed(function() {
+      classifier.save();
+    });
+  
+    // Load model
+    loadBtn = select('#load');
+    loadBtn.changed(function() {
+      classifier.load(loadBtn.elt.files, function(){
+        select('#modelStatus').html('Custom Model Loaded!');
+      });
+    });
+  }
+  
+  // Show the results
+  function gotResults(err, results) {
+    // Display any error
+    if (err) {
+      console.error(err);
+    }
+    if (results && results[0]) {
+      select('#result').html(results[0].label);
+      select('#confidence').html(results[0].confidence);
+      classify();
+    }
+  }
